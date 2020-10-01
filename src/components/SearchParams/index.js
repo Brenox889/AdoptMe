35f26db01107f9f7753836a66fd8dcbf57e6a4ee
brenox889 <brenomendes889@hotmail.com>
2020-10-01 19:49:30 -0300

    refactor: Home styles

--- src/components/SearchParams/index.js
+++ src/components/SearchParams/index.js
@@ -1,27 +1,30 @@
-import React, { useState } from 'react'
+import React, { useState } from 'react';
 
-import { Container } from './styles'
+import { Container } from './styles';
+
+import pet from '../../assets/icons/pawprint.png';
 
 export default function SearchParams() {
-  const [location, setLocation] = useState('')
+  const [location, setLocation] = useState('');
 
   return (
     <Container>
+      <img src={pet} alt="pet" />
       <form
         onSubmit={(e) => {
-          e.preventDefault()
+          e.preventDefault();
         }}
       >
-        <label htmlFor='location'>
+        <label htmlFor="location">
           Location
           <input
-            id='location'
-            placeholder='Location'
+            id="location"
+            placeholder="Location"
             value={location}
             onChange={(e) => setLocation(e.target.value)}
           />
         </label>
       </form>
     </Container>
-  )
+  );
 }
--- src/components/SearchParams/styles.js
+++ src/components/SearchParams/styles.js
@@ -1,15 +1,24 @@
-import styled from 'styled-components'
+import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 40%;
-  /* height: 440px; */
-  background-color: #661919;
+  width: 35vmax;
+  height: 41vmax;
+  background-color: #f1f1f1;
   display: flex;
-  margin: 2rem auto;
-  border-radius: 30px;
-  justify-content: center;
+  /* margin: 2rem auto; */
+  /* border-radius: 30px; */
+  /* justify-content: center; */
+  align-items: center;
 
+  > img {
+    width: 64px;
+    height: 64px;
+    align-self: flex-start;
+    /* justify-content: center; */
+    margin: 2rem 0 0 12rem;
+    position: absolute;
+  }
   input {
     width: 320px;
   }
-`
+`;
