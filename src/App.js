2113df388015c78b8373b04a59dad5c6e2f7a36c
brenox889 <brenomendes889@hotmail.com>
2020-10-01 19:49:56 -0300

    feat: Reach Router

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React from 'react';
 
 import SearchParams from './components/SearchParams';
+import Home from './components/Home';
 import NavBar from './components/NavBar';
 import NameProvider from './components/context/NameContext';
 
+import { Router } from '@reach/router';
+
 import './global.css';
+
+import background from './assets/budog3.jpeg';
 
 function App() {
   return (
     <NameProvider>
-      <div>
-        <NavBar />
-        <SearchParams />
+      <NavBar />
+      <div className="container">
+        <Router>
+          <SearchParams path="/" />
+        </Router>
+        <div className="img-Container">
+          <img src={background} alt="Adopt Me Background" />
+        </div>
       </div>
     </NameProvider>
   );
