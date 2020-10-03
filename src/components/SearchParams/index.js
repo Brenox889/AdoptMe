612dceef33b425268bb994f344f73236839ae0a2
brenox889 <brenomendes889@hotmail.com>
2020-10-02 23:36:49 -0300

    refactor: select on custom hooks

--- src/components/SearchParams/index.js
+++ src/components/SearchParams/index.js
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import { ANIMALS } from '@frontendmasters/pet';
+import useDropdown from '../useDropdown';
 
 import { Container } from './styles';
 
@@ -6,24 +8,38 @@ import pet from '../../assets/icons/pawprint.png';
 
 export default function SearchParams() {
   const [location, setLocation] = useState('');
+  const [animal, AnimalDropdown] = useDropdown('dog', ANIMALS);
+  const [breed, BreedDropdown] = useDropdown('breeds', breeds);
 
   return (
     <Container>
       <img src={pet} alt="pet" />
+      <span>Encontre o novo membro da familía</span>
       <form
         onSubmit={(e) => {
           e.preventDefault();
         }}
       >
-        <label htmlFor="location">
-          Location
-          <input
-            id="location"
-            placeholder="Location"
-            value={location}
-            onChange={(e) => setLocation(e.target.value)}
-          />
-        </label>
+        <input
+          id="location"
+          placeholder="Location"
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+        />
+        <AnimalDropdown />
+        <BreedDropdown />
+        {/* <select
+          value={animal}
+          onChange={(e) => setAnimal(e.target.value)}
+          onBlur={(e) => setAnimal(e.target.value)}
+        >
+          <option>Choose a Animal</option>
+          {ANIMALS.map((animal) => (
+            <option key={animal} value={animal}>
+              {animal}
+            </option>
+          ))}
+        </select> */}
       </form>
     </Container>
   );
