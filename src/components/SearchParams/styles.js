e86536c54a403f350847040a9a1cbe9dd6702b40
brenox889 <brenomendes889@hotmail.com>
2020-10-03 00:51:45 -0300

    style: form styles

--- src/components/SearchParams/styles.js
+++ src/components/SearchParams/styles.js
@@ -7,18 +7,55 @@ export const Container = styled.div`
   display: flex;
   /* margin: 2rem auto; */
   /* border-radius: 30px; */
-  /* justify-content: center; */
+  justify-content: center;
   align-items: center;
 
+  span {
+    font-size: 1.5rem;
+    color: #cc0001;
+    align-self: flex-start;
+    margin: 12rem 0 0 0;
+    position: absolute;
+    font-weight: 400;
+  }
+  span::after {
+    width: 30px;
+    height: 1px;
+    background-color: #cc0001;
+  }
   > img {
     width: 64px;
     height: 64px;
     align-self: flex-start;
     /* justify-content: center; */
-    margin: 2rem 0 0 12rem;
+    margin: 2rem 0 0 0;
     position: absolute;
   }
+  form {
+    margin: 4rem 0 0 0;
+    flex-wrap: wrap;
+    height: 20%;
+    display: flex;
+    align-self: center;
+    flex-direction: column;
+    align-items: center;
+    justify-content: space-between;
+  }
   input {
-    width: 320px;
+    width: 270px;
+    border: 1px solid #b8c6bf;
+    border-radius: 5px;
+    padding: 13px;
+    color: #333;
+  }
+  select {
+    width: 100%;
+    padding: 13px;
+    border: 1px solid #b8c6bf;
+    border-radius: 5px;
+    color: #757575;
+  }
+  option {
+    color: #333;
   }
 `;
