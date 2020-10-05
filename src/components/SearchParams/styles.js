1a2d65472f09a0ba16a78e53211d7d8a7b0e5651
brenox889 <brenomendes889@hotmail.com>
2020-10-04 23:46:43 -0300

    style: added and change components

--- src/components/SearchParams/styles.js
+++ src/components/SearchParams/styles.js
@@ -2,7 +2,7 @@ import styled from 'styled-components';
 
 export const Container = styled.div`
   width: 35vmax;
-  height: 41vmax;
+  height: 41.5vmax;
   background-color: #f1f1f1;
   display: flex;
   /* margin: 2rem auto; */
@@ -10,11 +10,17 @@ export const Container = styled.div`
   justify-content: center;
   align-items: center;
 
+  label {
+    font-size: 0.9rem;
+    color: #a3a3a3;
+    /* margin-right: 4rem; */
+    align-self: baseline;
+  }
   span {
     font-size: 1.5rem;
     color: #cc0001;
     align-self: flex-start;
-    margin: 12rem 0 0 0;
+    margin: 10rem 0 0 0;
     position: absolute;
     font-weight: 400;
   }
@@ -33,29 +39,51 @@ export const Container = styled.div`
   }
   form {
     margin: 4rem 0 0 0;
-    flex-wrap: wrap;
-    height: 20%;
+    width: 70%;
+    height: 35%;
     display: flex;
     align-self: center;
     flex-direction: column;
     align-items: center;
-    justify-content: space-between;
+    /* justify-content: space-between; */
   }
   input {
-    width: 270px;
+    width: 90%;
     border: 1px solid #b8c6bf;
     border-radius: 5px;
     padding: 13px;
     color: #333;
+    margin-bottom: 1rem;
   }
   select {
     width: 100%;
     padding: 13px;
     border: 1px solid #b8c6bf;
+    margin-bottom: 1rem;
     border-radius: 5px;
     color: #757575;
   }
   option {
     color: #333;
   }
+  button {
+    background-color: #c03440;
+    color: #f1f1f1;
+    font-weight: 300;
+    border-radius: 12px;
+    border-style: none;
+    width: 150px;
+    padding: 10px;
+    /* height: 35px; */
+    margin: 0.4rem 0 0 0;
+
+    cursor: pointer;
+    transition: 0.2s;
+
+    &:hover {
+      background-color: #942e3a;
+    }
+  }
 `;
+
+export const Result = styled.div``;
