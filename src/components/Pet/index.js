e893b054de0b1597af98d80b66497c9bfcb80936
brenox889 <brenomendes889@hotmail.com>
2020-10-04 22:52:23 -0300

    feat: Pet component

---
+++ src/components/Pet/index.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { Link } from '@reach/router';
+
+import { PetContainer, Info } from './styles';
+
+const Pet = (props) => {
+  const { name, animal, breed, media, location, id } = props;
+
+  let prof = 'http://placecorgi.com/300/300';
+
+  if (media.length) {
+    prof = media[0].small;
+  }
+  <PetContainer>
+    <Link to={`/details/${id}`}>
+      <figure>
+        <img src={prof} alt={name} />
+      </figure>
+      <Info>
+        <h1>{name}</h1>
+        <h2>{animal}</h2>
+        <p>{`${breed}-${location}`}</p>
+      </Info>
+    </Link>
+  </PetContainer>;
+};
